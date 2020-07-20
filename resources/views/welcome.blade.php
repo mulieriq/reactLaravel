def1ff3fb8c3248836b43fb00e7888381f2f79e8
Eric Muli <erickiokom@gmail.com>
2020-07-20 23:47:35 +0300

    First set app

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name' , 'ReactLaravel')}}</title>
+
+        <!-- Fonts -->
+        <link href="{{asset('css/app.css')}}" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Styles -->
+        </head>
+    <body>
+      <div id="example">
+      </div> 
+      <script src="{{asset('js/app.js')}}"></script>
+    </body>
+</html>
