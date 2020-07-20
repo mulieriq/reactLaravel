7eba6ef95cafe1bed7e54cd777fd1ca765cf615f
Eric Muli <erickiokom@gmail.com>
2020-07-21 00:39:11 +0300

    Added Header

---
+++ resources/js/components/header.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React from 'react';
+class Header extends Component{
+    render() {
+        return (
+             <div>Header</div>
+        );
+    }
+}
+export default Header;
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './header';
+
 
 function Index() {
     return (
         <div className="container">
+            <Header/>
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
