afa88f85c78a0d1016e8b75307d38b06a0e0fcb3
Eric Muli <erickiokom@gmail.com>
2020-07-23 15:15:44 +0300

    Created  additional screens

--- resources/js/components/header.js
+++ resources/js/components/header.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import About from "./about";
 import Home from "./home";
-
-
 class Header extends Component {
     render() {
         return (
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>{{config('app.name' , 'ReactLaravel')}}</title>
-
         <!-- Fonts -->
         <link href="{{asset('css/app.css')}}" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
